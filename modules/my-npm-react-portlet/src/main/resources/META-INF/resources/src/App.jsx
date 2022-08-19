7e4aab92599f2d0127badcdc011c700c850edda7
Grigoriy <vnukovg@gmail.com>
2022-08-19 14:49:24 -0700

    clean init index

--- modules/my-npm-react-portlet/src/main/resources/META-INF/resources/src/App.jsx
+++ modules/my-npm-react-portlet/src/main/resources/META-INF/resources/src/App.jsx
@@ -4,8 +4,9 @@ import ClayLink from '@clayui/link';
 const App = () => {
 	return (
 		<div className="row">
-			<ClayLink href="#link-styles" className="text-dark px-2">One(3)</ClayLink>
-			<ClayLink href="#link-styles" className="text-dark px-2">Two(1)</ClayLink>
+			<ClayLink href="#link-styles" className="text-dark px-2">one(3)</ClayLink>
+			<ClayLink href="#link-styles" className="text-dark px-2">two(1)</ClayLink>
+			<ClayLink href="#link-styles" className="text-dark px-2">three(1)</ClayLink>
 		</div>
 	)
 }
