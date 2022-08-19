fc0a8b2df40fde06df582ef74485e4a4f3aaef46
Grigoriy <vnukovg@gmail.com>
2022-08-19 14:18:03 -0700

    add clay

--- modules/my-npm-react-portlet/src/main/resources/META-INF/resources/src/App.jsx
+++ modules/my-npm-react-portlet/src/main/resources/META-INF/resources/src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import ClayLink from '@clayui/link';
 
 const App = () => {
-	return <h1>Hello 1</h1>
+	return (
+		<div className="row">
+			<ClayLink href="#link-styles" className="text-dark px-2">One(3)</ClayLink>
+			<ClayLink href="#link-styles" className="text-dark px-2">Two(1)</ClayLink>
+		</div>
+	)
 }
 
 export default App;
