414335bc8deb9459539e832e925efd5d5c81b258
Grigoriy <vnukovg@gmail.com>
2022-08-19 18:19:14 -0700

    return js folder

--- modules/my-npm-react-portlet/src/main/resources/META-INF/resources/js/App.jsx
+++ modules/my-npm-react-portlet/src/main/resources/META-INF/resources/js/App.jsx
@@ -4,9 +4,10 @@ import ClayLink from '@clayui/link';
 const App = () => {
 	return (
 		<div className="row">
-			<ClayLink href="#link-styles" className="text-dark px-2">one(3)</ClayLink>
+			<ClayLink href="#link-styles" className="text-dark px-2">one(2)</ClayLink>
 			<ClayLink href="#link-styles" className="text-dark px-2">two(1)</ClayLink>
 			<ClayLink href="#link-styles" className="text-dark px-2">three(1)</ClayLink>
+			<ClayLink href="#link-styles" className="text-dark px-2">four(1)</ClayLink>
 		</div>
 	)
 }
